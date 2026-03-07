57d54b053342d1fb797ae7052c0cb358b73fd48e
Mourvanhmaes <mourvanhmaes@gmail.com>
2026-03-07 20:20:15 -0300

    Pasta correta
    Dividi o projeto em pasta, fiz melhorias no código, adicionei algumas verificações

---
+++ src/main/java/org/example/infrastructure/persistence/DadosUsuario.java
@@ -0,0 +1,11 @@
+package org.example.infrastructure.persistence;
+
+import org.example.domain.model.Usuario;
+
+import java.util.ArrayList;
+
+public class DadosUsuario {
+
+    public static ArrayList<Usuario> usuario = new ArrayList<>();
+//    DadosUsuario.usuarios.add(new Usuario("todos os dados"));
+}
