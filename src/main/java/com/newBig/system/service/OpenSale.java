a89edd885e85e9938c7258766b78f5b343b9f803
Felipe Bocchi <felipebocchibazzanella@gmail.com>
2026-05-04 19:47:05 -0300

    feat: finalização as funcionalidade basicas para o front

--- src/main/java/com/newBig/system/service/OpenSale.java
+++ src/main/java/com/newBig/system/service/OpenSale.java
@@ -1,5 +1,7 @@
 package com.newBig.system.service;
 
+import com.newBig.system.domain.model.Cliente;
+import com.newBig.system.domain.model.Funcionario;
 import com.newBig.system.model.Sale;
 import com.newBig.system.repository.SaleInterface;
 
@@ -12,6 +14,8 @@ public class OpenSale {
 
     private SaleInterface repositorySale;
     private AddItemToSale addItemToSale;
+    // cliente
+    // funcionario
 
     public OpenSale(SaleInterface repositorySale, AddItemToSale addItemToSale) {
         this.repositorySale = repositorySale;
@@ -20,7 +24,19 @@ public class OpenSale {
 
     public Long execute(Long clienteId, Long funcionarioId, Long firstProductId, int quantity, String paymentMethod) {
 
+        // cliente
+        Cliente cliente = new Cliente();
+        // funcionario
+        Funcionario func = new Funcionario();
 
+        //  = Venda esqueleto que vai servir de ponto de início da venda
+        // !!! validações
+        Sale sale = new Sale(LocalDateTime.now(), cliente, func, BigDecimal.ZERO, paymentMethod, "ABERTA", new ArrayList<>());
+        repositorySale.save(sale);
+
+        addItemToSale.logic(sale.getId(), firstProductId, quantity);
+
+        return sale.getId();
     }
 
 
